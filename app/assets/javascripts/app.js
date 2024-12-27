0c085fc3f7d93819c9a7b29981f7e2110f9ee826
Diego-S-G <diego07goncalves@gmail.com>
2024-12-27 13:54:56 -0300

    The Lookup Function.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,2 +1,15 @@
 var app = angular.module('FinanceTrackerApp', [])
-                 .controller('stocksController', [$scope, function($scope){}])
+                 .controller('stocksController', [$scope, function($scope){
+                    $scope.lookup = function(){
+                        if($scope.ticker != undefined && $scope.ticker != ''){
+                            $scope.stock = { // mock por enquanto
+                                symbol: 'STH',
+                                name: 'Example Corp',
+                                last_price: 100.00
+                            }
+                        } else {
+                            $scope.stock = {}
+                        }
+                    }
+
+                 }])
