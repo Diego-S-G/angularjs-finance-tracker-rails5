5c78a8bb6fa61e022f0c8f125bcd25fda0b9d23d
Diego-S-G <diego07goncalves@gmail.com>
2024-12-27 10:32:11 -0300

    Apenas muda o comportamento do controller para não depender da API que não existe mais.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,7 +4,8 @@ class StocksController < ApplicationController
     if params[:stock].blank?
       flash.now[:danger] = "You have entered an empty search string"
     else
-      @stock = Stock.new_from_lookup(params[:stock])
+      # @stock = Stock.new_from_lookup(params[:stock]) # aqui faz a busca usando API mas não existe mais o site então fiz o básico p funcionar
+      @stock = Stock.find_by(ticker: params[:stock])
       flash.now[:danger] = "You have entered an incorrect symbol" unless @stock
     end
     respond_to do |format|
