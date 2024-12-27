b2d77d0f1e95a10443488d4cdcb1e7e826c8a790
Diego-S-G <diego07goncalves@gmail.com>
2024-12-27 14:47:03 -0300

    Binding the Results to the Lookup Function.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,15 +1,20 @@
 var app = angular.module('FinanceTrackerApp', [])
-                 .controller('stocksController', [$scope, function($scope){
-                    $scope.lookup = function(){
-                        if($scope.ticker != undefined && $scope.ticker != ''){
-                            $scope.stock = { // mock por enquanto
-                                symbol: 'STH',
-                                name: 'Example Corp',
-                                last_price: 100.00
-                            }
-                        } else {
-                            $scope.stock = {}
-                        }
-                    }
+    .controller('stocksController', ['$scope', function ($scope) {
 
-                 }])
+        $scope.stock = {};
+
+        $scope.lookup = function () {
+            if ($scope.ticker != undefined && $scope.ticker != '') {
+                $scope.stock = { // mock por enquanto
+                    symbol: 'STH',
+                    name: 'Example Corp',
+                    last_price: 100.00
+                }
+            } else {
+                $scope.stock = {
+                    symbol: {}
+                }
+            }
+        }
+
+    }])
